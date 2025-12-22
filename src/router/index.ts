312fc153819ff789a43b447d01ebe6147d8ff696
windyeasy <windyeasy13gb@163.com>
2025-12-22 18:25:48 +0800

    feat: 自动导入demo页面的路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,41 @@
+import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-export default createRouter({
-  routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/home',
-      component: () => import('@/views/home/index.vue'),
-    },
-  ],
-  history: createWebHashHistory(),
-})
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: () => import('@/views/home/index.vue'),
+  },
+]
+
+/**
+ * 自动加入demos的路由列表
+ * @param routes 默认的路由列表
+ * @returns 加入demos的路由列表
+ */
+function loadDemosRoutes(routes: RouteRecordRaw[]) {
+  const modules = import.meta.glob<any>('@/views/demos/**/*.vue')
+  Object.keys(modules).forEach((key: string) => {
+    const match = key.match(/\/views\/(demos\/[^/]+)\//)
+    if (match) {
+      routes.push({
+        path: `/${match[1]}`,
+        component: modules[key],
+      })
+    }
+  })
+
+  return routes
+}
+
+export default function newCreateRouter(app: any) {
+  const router = createRouter({
+    routes: loadDemosRoutes(routes),
+    history: createWebHashHistory(),
+  })
+  app.use(router)
+}
---
+++ src/views/demos/powerscreen/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// todo: 可以配置title， cover，直接通过store得到demo的信息不需要每次都在home/data.ts里面写入
+</script>
+
+<template>
+  <div>
+    power-screen
+  </div>
+</template>
