e62b7ad7308783e00be1482fbeb373367b6662c8
windyeasy <windyeasy13gb@163.com>
2025-12-24 18:46:30 +0800

    feat: 添加要使用的图片

---
+++ src/views/demos/powerscreen/cpns/LeftTop.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="screen-left-top w-full h-452px">
+    <div class="inner pt-50px  text-amber">
+      left
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.screen-left-top {
+  background: url('@/assets/images/bg_left-top.svg') no-repeat;
+  background-size: contain;
+
+}
+</style>
