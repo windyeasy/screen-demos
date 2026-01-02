802bef87f681c6d47eb2fc65f6653197952df72c
windyeasy <windyeasy13gb@163.com>
2026-01-02 20:11:03 +0800

    feat: picscreen首页的基础布局

---
+++ src/components/ScreenPanel.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { BorderBox1 } from '@kjgl77/datav-vue3'
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <BorderBox1>
+    <div class="screen-panel-inner box-border flex flex-col w-full h-full ">
+      <div class="title flex pl-6 pt-7 font-bold justify-start w-full  c-[#0092d0]">
+        <span class="title-icon" />
+        <span class="text pl-5">{{ title }}</span>
+      </div>
+      <div class="content flex-1 m-4 box-border">
+        <slot />
+      </div>
+    </div>
+  </BorderBox1>
+</template>
+
+<style scoped>
+  .title-icon {
+    position: relative;
+    &::before, &::after {
+      position: absolute;
+      content: '';
+      width: 8px;
+      height: 8px;
+      border: 3px solid #00bde4;
+    }
+    &::before {
+      border-left-color: transparent;
+      border-bottom-color: transparent;
+      transform: rotate(45deg);
+      left: -10px;
+      top: 2px;
+    }
+     &::after {
+      border-left-color: transparent;
+      border-bottom-color: transparent;
+      transform: rotate(45deg);
+      left: -3px;
+      top: 2px;
+    }
+  }
+</style>
---
+++ src/views/demos/picscreen/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import useScreenScale from '@/composables/useScreenScale'
+
+useScreenScale()
+</script>
+
+<template>
+  <div class="wrapper flex flex-col">
+    <ScreenHeader title="上海摄网大数据统计" />
+    <main class="screen-main flex flex-col  flex-1 mt-6 w-full px-6 box-border">
+      <div class="main-top flex  w-full flex-1 ">
+        <div class="main-top-left flex-1 h-full">
+          <ScreenPanel title="全市写字楼空置率分析" />
+        </div>
+        <div class="main-top-center flex-1 h-full">
+          <ScreenPanel title="入住上市企业数量" />
+        </div>
+        <div class="main-top-right flex-1 h-full">
+          <ScreenPanel title="全市当地注册率" />
+        </div>
+      </div>
+      <div class="main-bottom flex  flex-1 mt-8 mb-10">
+        <div class="main-bottom-left flex-1 h-full">
+          <ScreenPanel title="入驻世界500强企业数量" />
+        </div>
+        <div class="main-bottom-right flex-1 h-full">
+          <ScreenPanel title="全市写字楼租售面积比" />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  background: url('@/assets/images/bg.png') no-repeat;
+  background-size: cover;
+}
+</style>
